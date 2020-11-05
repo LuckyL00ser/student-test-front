59f2cd10e89a65695d5f4d3a3c376b5a20f8d819
Kamil Wlazlo <kwlazlo10@gmail.com>
2020-11-05 20:35:17 +0100

    Single choice question component

---
+++ src/components/SingleChoiceQuestion.vue
@@ -0,0 +1,96 @@
+<template>
+	<b-container fluid>
+		<b-row class="mt-2">
+			<b-col sm="2">
+				<label for="txt-question" class="float-left">Pytanie:</label>
+			</b-col>
+			<b-col>
+				<input
+					id="txt-question"
+					v-model="question"
+					placeholder="Pytanie..."
+					class="col-12"
+				/>
+			</b-col>
+			<b-col sm="2">
+				<b-button class="btn btn-success float-right" @click="addQuestion()">
+					Dodaj odpowiedź
+				</b-button>
+			</b-col>
+		</b-row>
+		<b-row v-for="(answer, index) in answers" :key="index" class="mt-2">
+			<b-col sm="2">
+				<label for="txt-answer" class="text-nowrap float-left">
+					Odpowiedź {{ index + 1 }}:
+				</label>
+			</b-col>
+			<b-col>
+				<input
+					id="txt-answer"
+					v-model="answers[index]"
+					type="text"
+					placeholder="Odpowiedź..."
+					class="col-12"
+				/>
+			</b-col>
+			<b-col sm="1">
+				<b-checkbox :id="'box-'+index" v-model="isCorrect[index]" class="text-nowrap" size="lg" @change="validate(index)"/>
+			</b-col>
+			<b-col sm="1">
+				<b-button class="btn btn-danger btn-sm" @click="deleteQuestion(index)">
+					X
+				</b-button>
+			</b-col>
+		</b-row>
+	</b-container>
+</template>
+
+<script>
+export default {
+	name: 'SingleChoiceQuestion',
+	data: function() {
+		return {
+			question: '',
+			answers: [''],
+			isCorrect: [false],
+		};
+	},
+	methods: {
+		addQuestion() {
+			this.answers.push('');
+			this.isCorrect.push(false);
+		},
+
+		deleteQuestion(index) {
+			if (this.answers.length > 1) {
+				this.answers.splice(index, 1);
+				this.isCorrect.splice(index, 1);
+			} else {
+				this.$store.toast(
+					'danger',
+					'Pytanie musi zawierać co najmniej jedną odpowiedź.',
+				);
+			}
+        },
+        
+        validate(index) {
+            var checkBox = document.getElementById("box-" + index);
+            var items = document.querySelectorAll("div.container-fluid input[type='checkbox']")
+            if(checkBox.checked) {
+                items.forEach(element => {
+                    if (element.checked) 
+                        return;
+                    element.disabled = true;
+                })
+            } else {
+                items.forEach(element => {
+                    element.disabled = false;
+                });
+            }
+
+        }
+	},
+};
+</script>
+
+<style scoped></style>
